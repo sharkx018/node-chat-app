69f071c2b3185ee84f79140e8ee6c60660a1815b
robof1 <aryanv018@gmail.com>
2019-11-09 00:32:36 +0530

    test

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const express = require('express');
-var app = express();
+
 
 var loc = path.join(__dirname,'/../public');
+const port = process.env.PORT || 3001;
+var app = express();
+
+
 app.use( express.static(loc));
 
 //console.log(path.join(__dirname,'/../public'));
 
-app.listen(3000, ()=>{
-	console.log('Server is running on port: 3000');
+app.listen(port, ()=>{
+	console.log('Server is running on port: '+port);
 });
