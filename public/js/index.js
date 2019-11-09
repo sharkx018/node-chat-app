1107649a2fc0dd768a1eb34e024e280f92833413
robof1 <aryanv018@gmail.com>
2019-11-10 03:51:55 +0530

    Add form for messages and show incomming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -4,14 +4,28 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('Connected to server.');
 });
-
-
 socket.on('disconnect', function(){
 	console.log('disconnected from server');
 });
  
 
+
+
 socket.on('newMessage', (msg)=>{
 	console.log('newMessage',msg);
+	var li = jQuery('<li></li>');
+	li.text(msg.from +": "+msg.text);
+	jQuery('#msg-list').append(li);
+});
+
+
+jQuery('#message-form').on('submit', function(e){
+	e.preventDefault();
+	socket.emit('createMessage', {
+		from:'User',
+		text:jQuery('[name=message]').val()
+	}, function(d){
+		//console.log(d);
+	} )
 });
 		
--- server/server.js
+++ server/server.js
@@ -12,23 +12,18 @@ var io = socketIO(server);
 
 app.use( express.static(path.join(__dirname,'/../public')));
 
-io.on('connection', function(socket){
+io.on('connection', function(socket)
+{
+
 	console.log('New user connected!');
-
 	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-	socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user added to the chat app'));
+	socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user added'));
 
-	socket.on('createMessage', function(msg){
+	socket.on('createMessage', function(msg, callback){
 		console.log('createMessage',msg);
-
-		// io.emit('newMessage', {
-		// 	from:msg.from,
-		// 	text:msg.text,
-		// 	createdAt:new Date().getTime()
-		// });
-
+		// io.emit('newMessage', generateMessage(msg.from, msg.text));
 		socket.broadcast.emit('newMessage', generateMessage(msg.from, msg.text));
-
+		callback('Delieved!');
 
 	});
 
@@ -36,27 +31,7 @@ io.on('connection', function(socket){
 		console.log('User disconnected');
 	});
 
-	// socket.on('add', function(msg){
-	// 	console.log("New User ", msg.name);
-
-	// 	socket.broadcast.emit('newMessage', {
-	// 		text:msg.name +' added!'
-	// 	});
-
-	// 	socket.emit('newMessage',{
-	// 		text:'Welcome '+ msg.name+' !'
-	// 	});
-
-	// });
-
-	// socket.emit('newMessage', {
-	// 	from:"server@gmail.com",
-	// 	text:"hello this is reply",
-	// 	createdAt: new Date().getTime()
-	// });
-
-
-
+	
 });
 
 server.listen(port, function(){
