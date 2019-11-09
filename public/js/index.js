2da3f8473f3ec413540e936a4a277320db01489b
robof1 <aryanv018@gmail.com>
2019-11-09 13:48:29 +0530

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -4,17 +4,12 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('Connected to server.');
 });
+
+
 socket.on('disconnect', function(){
 	console.log('disconnected from server');
 });
-
-
-
-// socket.emit('createMessage',{
-// 	from:"mukul@gmail.com",
-// 	text:"Hello i am mukul!"
-// } );
-
+ 
 
 socket.on('newMessage', (msg)=>{
 	console.log('newMessage',msg);
--- server/server.js
+++ server/server.js
@@ -14,23 +14,49 @@ app.use( express.static(path.join(__dirname,'/../public')));
 io.on('connection', function(socket){
 	console.log('New user connected!');
 
-	socket.on('disconnect', function(){
-		console.log('User disconnected');
+	socket.emit('newMessage', {
+		from:"Admin",
+		text:"Welcome to the chat app from admin"
+	})
+	socket.broadcast.emit('newMessage',{
+		from:"Admin",
+		text:"New User added!"
 	});
 
-	
-	
 	socket.on('createMessage', function(msg){
-		console.log(' createMessage',msg);
+		console.log('createMessage',msg);
 
-		io.emit('newMessage', {
-			from:msg.from,
-			text:msg.text,
-			createdAt:new Date().getTime()
+		// io.emit('newMessage', {
+		// 	from:msg.from,
+		// 	text:msg.text,
+		// 	createdAt:new Date().getTime()
+		// });
+
+		socket.broadcast.emit('newMessage', {
+			from: msg.from,
+			text: msg.text,
+			createdAt: new Date().getTime()
 		});
 
 
 	});
+
+	socket.on('disconnect', function(){
+		console.log('User disconnected');
+	});
+
+	// socket.on('add', function(msg){
+	// 	console.log("New User ", msg.name);
+
+	// 	socket.broadcast.emit('newMessage', {
+	// 		text:msg.name +' added!'
+	// 	});
+
+	// 	socket.emit('newMessage',{
+	// 		text:'Welcome '+ msg.name+' !'
+	// 	});
+
+	// });
 
 	// socket.emit('newMessage', {
 	// 	from:"server@gmail.com",
